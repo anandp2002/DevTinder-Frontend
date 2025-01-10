323a74c894032ae1ffe096ea24fdd0860304ed94
Anand P <anandpamegoze@gmail.com>
2025-01-10 20:11:36 +0530

    basic navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,43 @@
+import LOGO from '../../public/devTinder.jpg';
+const Navbar = () => {
+  return (
+    <div className="navbar bg-base-300">
+      <div className="flex-1 flex items-center">
+        <img src={LOGO} className="h-8 w-8" />
+        <a className="btn btn-ghost text-xl">daisyUI</a>
+      </div>
+      <div className="flex-none gap-2">
+        <div className="dropdown dropdown-end">
+          <div
+            tabIndex={0}
+            role="button"
+            className="btn btn-ghost btn-circle avatar"
+          >
+            <div className="w-10 rounded-full">
+              <img
+                alt="Tailwind CSS Navbar component"
+                src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp"
+              />
+            </div>
+          </div>
+          <ul
+            tabIndex={0}
+            className="menu menu-sm dropdown-content bg-base-300 rounded-box z-[1] mt-3 w-52 p-2 shadow-lg"
+          >
+            <li>
+              <a>Profile</a>
+            </li>
+            <li>
+              <a>Settings</a>
+            </li>
+            <li>
+              <a>Logout</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
