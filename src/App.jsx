322401663dce57196ef371156263284997c4b6a3
Anand P <anandpamegoze@gmail.com>
2025-01-20 11:41:33 +0530

    configure reduxt tool kit

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,21 @@ import Login from './components/Login';
 import { Routes, Route, BrowserRouter } from 'react-router-dom';
 import Profile from './components/Profile';
 import Body from './components/Body';
+import { Provider } from 'react-redux';
+import appStore from './utils/appStore';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Body />}>
-          <Route path="/login" element={<Login />}></Route>
-          <Route path="/profile" element={<Profile />}></Route>
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <Provider store={appStore}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Body />}>
+            <Route path="/login" element={<Login />}></Route>
+            <Route path="/profile" element={<Profile />}></Route>
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </Provider>
   );
 }
 
