52d720366e5bd8bdb4edb9c2f0a9526796fea05a
Anand P <anandpamegoze@gmail.com>
2025-01-24 20:29:44 +0530

    image upload in cloudinary

--- src/components/EditProfile.jsx
+++ src/components/EditProfile.jsx
@@ -6,6 +6,12 @@ import { useDispatch } from 'react-redux';
 import { addUser } from '../utils/userSlice';
 import { toast } from 'react-hot-toast';
 import { useNavigate } from 'react-router-dom';
+import {
+  CLOUDINARY_URL,
+  CLOUDINARY_PRESET,
+  CLOUDINARY_FOLDER,
+} from '../utils/constants';
+import { Loader } from 'lucide-react';
 
 const EditProfile = ({ user }) => {
   const [firstName, setFirstName] = useState(user.firstName);
@@ -15,12 +21,36 @@ const EditProfile = ({ user }) => {
   const [gender, setGender] = useState(user.gender || '');
   const [about, setAbout] = useState(user.about || '');
   const [error, setError] = useState('');
+  const [uploading, setUploading] = useState(false);
+  const [saving, setSaving] = useState(false); // Track save operation
   const dispatch = useDispatch();
   const navigate = useNavigate();
 
+  const handleImageUpload = async (e) => {
+    const file = e.target.files[0];
+    if (!file) return;
+
+    const formData = new FormData();
+    formData.append('file', file);
+    formData.append('upload_preset', CLOUDINARY_PRESET);
+    formData.append('folder', CLOUDINARY_FOLDER);
+
+    try {
+      setUploading(true);
+      const res = await axios.post(CLOUDINARY_URL, formData);
+      setPhotoUrl(res.data.secure_url); // Set the uploaded image URL
+      toast.success('Image updated successfully !');
+    } catch (err) {
+      console.error(err);
+      toast.error('Failed to update image, Try again !');
+    } finally {
+      setUploading(false);
+    }
+  };
+
   const saveProfile = async () => {
-    // Clear Errors
     setError('');
+    setSaving(true); // Start the saving state
     try {
       const res = await axios.put(
         BASE_URL + '/profile/edit',
@@ -35,115 +65,129 @@ const EditProfile = ({ user }) => {
         { withCredentials: true }
       );
       dispatch(addUser(res?.data?.data));
-      toast.success('Profile saved successfully !');
+      toast.success('Profile saved successfully!');
+      navigate('/');
     } catch (err) {
-      setError(err.response.data);
+      setError(err.response?.data || 'Something went wrong!');
+    } finally {
+      setSaving(false); // Stop the saving state
     }
   };
 
   return (
-    <>
-      <div className="flex flex-col lg:flex-row items-start justify-center my-8 gap-8 w-full">
-        <div className="flex justify-center max-w-xl px-4 w-full">
-          <div className="card bg-base-300 w-full shadow-xl">
-            <div className="card-body">
-              <h2 className="card-title text-blue-100 font-bold justify-center">
-                Edit your Profile
-              </h2>
-              <div>
-                <label className="form-control w-full my-2">
-                  <div className="label">
-                    <span className="label-text">First Name</span>
+    <div className="flex flex-col lg:flex-row items-start justify-center my-8 gap-8 w-full">
+      <div className="flex justify-center max-w-xl px-4 w-full">
+        <div className="card bg-base-300 w-full shadow-xl">
+          <div className="card-body">
+            <h2 className="card-title text-blue-100 font-bold justify-center">
+              Edit your Profile
+            </h2>
+            <div>
+              <label className="form-control w-full my-2">
+                <div className="label">
+                  <span className="label-text">First Name</span>
+                </div>
+                <input
+                  type="text"
+                  value={firstName}
+                  className="input input-bordered w-full"
+                  onChange={(e) => setFirstName(e.target.value)}
+                />
+              </label>
+              <label className="form-control w-full my-2">
+                <div className="label">
+                  <span className="label-text">Last Name</span>
+                </div>
+                <input
+                  type="text"
+                  value={lastName}
+                  className="input input-bordered w-full"
+                  onChange={(e) => setLastName(e.target.value)}
+                />
+              </label>
+              <label className="form-control w-full my-2">
+                <div className="label">
+                  <span className="label-text">Photo</span>
+                </div>
+                <input
+                  type="file"
+                  accept="image/*"
+                  className="file-input file-input-bordered w-full"
+                  onChange={handleImageUpload}
+                />
+                {uploading && (
+                  <div className="flex items-center justify-center gap-2 mt-4">
+                    <Loader className="animate-spin text-white size-5" />
+                    Uploading...
                   </div>
-                  <input
-                    type="text"
-                    value={firstName}
-                    className="input input-bordered w-full"
-                    onChange={(e) => setFirstName(e.target.value)}
-                  />
-                </label>
-                <label className="form-control w-full my-2">
-                  <div className="label">
-                    <span className="label-text">Last Name</span>
-                  </div>
-                  <input
-                    type="text"
-                    value={lastName}
-                    className="input input-bordered w-full"
-                    onChange={(e) => setLastName(e.target.value)}
-                  />
-                </label>
-                <label className="form-control w-full my-2">
-                  <div className="label">
-                    <span className="label-text">Photo URL</span>
-                  </div>
-                  <input
-                    type="text"
-                    value={photoUrl}
-                    className="input input-bordered w-full"
-                    onChange={(e) => setPhotoUrl(e.target.value)}
-                  />
-                </label>
-                <label className="form-control w-full my-2">
-                  <div className="label">
-                    <span className="label-text">Age</span>
-                  </div>
-                  <input
-                    type="number"
-                    value={age}
-                    className="input input-bordered w-full"
-                    onChange={(e) => setAge(e.target.value)}
-                  />
-                </label>
-                <label className="form-control w-full my-2">
-                  <div className="label">
-                    <span className="label-text">Gender</span>
-                  </div>
-                  <select
-                    value={gender}
-                    className="select select-bordered w-full"
-                    onChange={(e) => setGender(e.target.value)}
-                  >
-                    <option value="">select</option>
-                    <option value="male">male</option>
-                    <option value="female">female</option>
-                    <option value="other">other</option>
-                  </select>
-                </label>
-                <label className="form-control w-full my-2">
-                  <div className="label">
-                    <span className="label-text">About</span>
-                  </div>
-                  <textarea
-                    value={about}
-                    className="textarea textarea-bordered w-full"
-                    onChange={(e) => setAbout(e.target.value)}
-                  />
-                </label>
-              </div>
-              <p className="text-red-500 text-sm">{error}</p>
-              <div className="card-actions justify-center mt-4">
-                <button
-                  className="btn font-bold bg-[#316FF6] hover:bg-blue-600 text-white w-full"
-                  onClick={() => {
-                    saveProfile();
-                    navigate('/');
-                  }}
+                )}
+              </label>
+              <label className="form-control w-full my-2">
+                <div className="label">
+                  <span className="label-text">Age</span>
+                </div>
+                <input
+                  type="number"
+                  value={age}
+                  className="input input-bordered w-full"
+                  onChange={(e) => setAge(e.target.value)}
+                />
+              </label>
+              <label className="form-control w-full my-2">
+                <div className="label">
+                  <span className="label-text">Gender</span>
+                </div>
+                <select
+                  value={gender}
+                  className="select select-bordered w-full"
+                  onChange={(e) => setGender(e.target.value)}
                 >
-                  Save
-                </button>
-              </div>
+                  <option value="">select</option>
+                  <option value="male">male</option>
+                  <option value="female">female</option>
+                  <option value="other">other</option>
+                </select>
+              </label>
+              <label className="form-control w-full my-2">
+                <div className="label">
+                  <span className="label-text">About</span>
+                </div>
+                <textarea
+                  value={about}
+                  className="textarea textarea-bordered w-full"
+                  onChange={(e) => setAbout(e.target.value)}
+                />
+              </label>
+            </div>
+            <p className="text-red-500 text-sm">{error}</p>
+            <div className="card-actions justify-center mt-4">
+              <button
+                className={`btn font-bold ${
+                  saving ? 'bg-gray-400' : 'bg-[#316FF6] hover:bg-blue-600'
+                } text-white w-full`}
+                onClick={saveProfile}
+                disabled={saving} // Disable button while saving
+              >
+                {saving ? (
+                  <div className="flex items-center justify-center gap-2">
+                    <Loader className="animate-spin text-white size-5" />
+                    Saving...
+                  </div>
+                ) : (
+                  'Save'
+                )}
+              </button>
             </div>
           </div>
         </div>
-        <div className="hidden justify-between lg:block px-4 w-auto -mt-4">
-          <UserCard
-            isProfilePage={true}
-            user={{ firstName, lastName, photoUrl, age, gender, about }}
-          />
-        </div>
       </div>
-    </>
+      <div className="hidden justify-between lg:block px-4 w-auto -mt-4">
+        <UserCard
+          isProfilePage={true}
+          user={{ firstName, lastName, photoUrl, age, gender, about }}
+        />
+      </div>
+    </div>
   );
 };
 
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,5 +1,11 @@
 // export const BASE_URL = 'http://localhost:3000';
 
-export const BASE_URL = 'http://192.168.1.3:3000';
+export const BASE_URL = 'http://192.168.1.2:3000';
 
 // export const BASE_URL = 'http://192.168.191.74:3000';
+
+export const CLOUDINARY_CLOUD_NAME = 'dnh4vxs5r';
+export const CLOUDINARY_URL =
+  'https://api.cloudinary.com/v1_1/' + CLOUDINARY_CLOUD_NAME + '/upload';
+export const CLOUDINARY_PRESET = 'devTinder';
+export const CLOUDINARY_FOLDER = 'devTinder';
