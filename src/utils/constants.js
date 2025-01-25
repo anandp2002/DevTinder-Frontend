3638caabb07a01ea75d44c195eb70c3a7b16898b
Anand P <anandpamegoze@gmail.com>
2025-01-25 14:07:33 +0530

    add .env configuration

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,11 +1,9 @@
 // export const BASE_URL = 'http://localhost:3000';
 
-export const BASE_URL = 'http://192.168.1.2:3000';
-
 // export const BASE_URL = 'http://192.168.191.74:3000';
 
-export const CLOUDINARY_CLOUD_NAME = 'dnh4vxs5r';
-export const CLOUDINARY_URL =
-  'https://api.cloudinary.com/v1_1/' + CLOUDINARY_CLOUD_NAME + '/upload';
-export const CLOUDINARY_PRESET = 'devTinder';
-export const CLOUDINARY_FOLDER = 'devTinder';
+export const BASE_URL = import.meta.env.VITE_BASE_URL;
+export const CLOUDINARY_CLOUD_NAME = import.meta.env.VITE_CLOUDINARY_CLOUD_NAME;
+export const CLOUDINARY_URL = import.meta.env.VITE_CLOUDINARY_URL;
+export const CLOUDINARY_PRESET = import.meta.env.VITE_CLOUDINARY_PRESET;
+export const CLOUDINARY_FOLDER = import.meta.env.VITE_CLOUDINARY_FOLDER;
