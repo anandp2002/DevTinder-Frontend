1680452187a70996512109a89f8e096e78d66928
Anand P <anandpamegoze@gmail.com>
2025-01-29 16:20:55 +0530

    change shimmer style

--- src/components/UserCardShimmer.jsx
+++ src/components/UserCardShimmer.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const UserCardShimmer = () => {
   return (
     <div className="card bg-base-300 shadow-xl m-4 animate-pulse">
-      <div className="h-80 w-80 sm:h-[340px] sm:w-[340px] bg-gray-700 rounded" />
+      <div className="h-80 w-80 sm:h-[340px] sm:w-[340px] bg-gray-700 rounded-t-xl" />
       <div className="p-4">
         <div className="h-6 w-40 bg-gray-700 rounded mb-2"></div>
         <div className="h-4 w-20 bg-gray-700 rounded"></div>
